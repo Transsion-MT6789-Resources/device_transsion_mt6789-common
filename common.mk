5c59621a3e7f006dbb881c7bd2ec483e8896c4da
Miao Wang <miaowang@google.com>
2017-05-24 15:27:38 -0700

    mt6789-common: Add RenderScript default implementation to device.mk
    Bug: 37035983
    
      - OVERRIDE_RS_DRIVER is moved from libRS_internal to the default
      implmentation. In order for GPU driver to work on legacy devices, the
      default implementation needs to be part of PRODUCT_PACKAGES.
    
    Test: mm
    Test: CTS tests pass on Bullhead, and confirm GPU driver is working.
    Change-Id: I72e6d5a36b267d77412801f9c02894a8af643d84
    Signed-off-by: dodyirawan85 <dev.irawans@gmail.com>

--- common.mk
+++ common.mk
@@ -245,6 +245,10 @@ PRODUCT_PACKAGES += \
     android.hardware.radio.config@1.3.vendor \
     android.hardware.radio@1.6.vendor
 
+# RenderScript HAL
+PRODUCT_PACKAGES += \
+    android.hardware.renderscript@1.0-impl
+
 # Secure Element
 PRODUCT_PACKAGES += \
     android.hardware.secure_element@1.2.vendor
