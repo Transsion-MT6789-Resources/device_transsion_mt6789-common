c44ba8bc78a20dd334b359105f776890013b4094
LuK1337 <priv.luk@gmail.com>
2021-01-06 19:30:19 +0100

    mt6789-common: Build vndservicemanager
    * Needed for FCM >= R.
    
    Change-Id: Ibc27a98f5774a51caaab077c1d8e0a2a79bdffe5
    Signed-off-by: dodyirawan85 <dev.irawans@gmail.com>

--- common.mk
+++ common.mk
@@ -271,6 +271,10 @@ PRODUCT_PACKAGES += \
     android.hardware.usb@1.3.vendor \
     android.hardware.usb.gadget@1.1.vendor
 
+# Vendor service manager
+PRODUCT_PACKAGES += \
+    vndservicemanager
+
 # Wi-Fi
 PRODUCT_PACKAGES += \
     wpa_supplicant \
